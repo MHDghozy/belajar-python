b7712900f51390fa3b7f6137dd84f602138385c1
Ghozy <muhammad.ghozy@student.alhazen.academy>
2023-11-10 17:15:03 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,9 @@
+from stringcolor import *
+from emoji import *
+
+print(cs("Muhammad Ghozy Alghifary","red"))
+print(bold("Selamat Datang"))
+print(cs("Ghozy", "gold").underline())
+print(cs("Ghozy", "blue", "white"))
+
+print(emojize(":pig:"))
